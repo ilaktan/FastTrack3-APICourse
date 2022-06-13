eb6d492d6f99a974c4089effa9733bedcc81d699
Zulpikar <zulpikaruygur@cybertekschool.com>
2022-06-14 01:52:30 +0300

    annotations is added

--- src/test/java/day5/Annotations.java
+++ src/test/java/day5/Annotations.java
@@ -4,8 +4,43 @@ import org.junit.jupiter.api.*;
 
 public class Annotations {
 
+
+
     @BeforeAll
     public static void init(){
         System.out.println("Before all is running");
     }
+
+    @AfterAll
+    public static void after(){
+        System.out.println("After all is running");
+
+    }
+
+    @BeforeEach
+    public void bEach(){
+        System.out.println("Before each is running");
+
+    }
+
+    @AfterEach
+    public void aEach(){
+        System.out.println("AFter each is running");
+
+    }
+
+    @DisplayName("My first test")
+    @Test
+    public void test1()
+    {
+        System.out.println("test 1 is running");
+    }
+
+    @Disabled
+    @Test
+    public void test2()
+    {
+        System.out.println("test 2 is running");
+    }
+
 }
